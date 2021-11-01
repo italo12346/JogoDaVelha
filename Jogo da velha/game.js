54ecd6d6e3455f5b6c69bf7bc79f614d757d6019
Italo souza sampaio <78382234+italo12346@users.noreply.github.com>
2021-11-01 18:33:03 -0300

    Add files via upload

---
+++ Jogo da velha/game.js
@@ -0,0 +1,51 @@
+const player1 ="x";
+const player2 ="O";
+var playTime = player1;
+var gameOver = false;
+
+atualizaMostrador();
+
+inicializar();
+function atualizaMostrador(){
+
+    if(gameOver){return;}
+    
+    if (playTime == player1) {
+        var player = document.querySelectorAll("div#mostrar img")[0];
+        player.setAttribute("src","imagens/x.png");
+    }else{
+        var player = document.querySelectorAll("div#mostrador img")[0];
+        player.setAttribute("src", "imagens/o.png");
+    }
+
+
+
+}
+
+function inicializar(){
+    var espacos = document.getElementsByClassName('espaco');
+    for (let i = 0; i < espacos.length; i++) {
+        espacos[i].addEventListener("click", function(){
+
+            if(gameOver) {return;}
+
+            if(this.getElementsByTagName("img").length == 0){
+            
+                if(playTime == player1){
+                    this.innerHTML = "<img src='imagens/x.png'>";
+                    this.setAttribute("jogada",player1);
+                    playTime= player2;
+                }else{
+
+                    this.innerHTML = "<img src='imagens/o.png'>";
+                    this.setAttribute("jogada",player1);
+                    playTime= player1;
+
+
+                }
+                atualizaMostrador();
+            }
+        }
+        
+    }
+}
